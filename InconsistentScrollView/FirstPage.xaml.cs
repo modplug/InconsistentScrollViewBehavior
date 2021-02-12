b3dc2974318ba38be08534a3b9be5089441860c0
Erlend Angelsen <erlend.angelsen@gmail.com>
2021-02-12 14:17:34 +0100

    Initial commit

---
+++ InconsistentScrollView/FirstPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace InconsistentScrollView
+{
+    public partial class FirstPage : ContentPage
+    {
+        public FirstPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
